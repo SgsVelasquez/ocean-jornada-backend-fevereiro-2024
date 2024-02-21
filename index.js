ebe56d03f112f6e6cc8e22f50f993ba838e22c23
Sérgio Velasquez <47956760+SgsVelasquez@users.noreply.github.com>
2024-02-21 14:41:10 +0000

    rota item by id criado

--- index.js
+++ index.js
@@ -9,4 +9,16 @@ app.get('/oi', function (req, res) {
     res.send("Hello World!");
 })
 
+const lista = ['Rick', 'Morty', 'Summer']
+
+app.get('/item', function(req, res) {
+  res.send(lista)
+})
+
+app.get('/item/:id', function(req, res) {
+  const id = req.params.id
+  const item = lista[id]
+  res.send(item)
+})
+
 app.listen(3000)
