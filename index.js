f085ecf816a628a9f2dfec6efad9992bbd8c81a8
Sérgio Velasquez <47956760+SgsVelasquez@users.noreply.github.com>
2024-02-28 12:49:50 +0000

    Banco de dados implementado no endpoint Read By ID

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const dbURL = 'mongodb+srv://admin:NMq6WrHl3qSjFPat@cluster0.jf4xsss.mongodb.net/'
 const dbname = 'OceanJornadaBackendFev2024'
 
 async function main() {
-  const client = new MongoClient(dbUrl)
+  const client = new MongoClient(dbURL)
 
   console.log('Conectando ao banco de dados...')
   await client.connect()
@@ -25,7 +25,7 @@ async function main() {
   const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
   //              0               1              2
 
-  const db = client.db(dbName)
+  const db = client.db(dbname)
   const collection = db.collection('items')
 
   // Read All -> [GET] /item
@@ -38,12 +38,14 @@ async function main() {
   })
 
   // Read By ID -> [GET] /item/:id
-  app.get('/item/:id', function (req, res) {
+  app.get('/item/:id', async function (req, res) {
     // Acesso o ID no parâmetro de rota
     const id = req.params.id
 
-    // Acesso item na lista baseado no ID recebido
-    const item = lista[id]
+    // Acesso item na collection baseado no ID recebido
+    const item = await collection.findOne({
+      _id: new ObjectId(id)
+    })
 
     // Envio o item obtido como resposta HTTP
     res.send(item)
